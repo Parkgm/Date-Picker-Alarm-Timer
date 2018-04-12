753e19e837d528bbfb4337296d8a7f6bed738b0f
D7703_13 <d7703_13@d7703-13ui-imac.local>
2018-04-12 15:53:20 +0900

    Date Picker

--- Date Picker Alarm Timer/ViewController.swift
+++ Date Picker Alarm Timer/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var currentTime: UILabel!
+    @IBOutlet weak var setTime: UILabel!
+    
+    var myTimer = Timer()
+    
+    
+ 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       //현재 시간 넣어야함
+        
+        
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func updateTime() {_ = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        currentTime.text = formatter.string(from: datePicker.date)
+        //        timeLabel.text = String(describing: date)
+        
+        if currentTime.text == setTime.text {
+            view.backgroundColor = UIColor.red
+        }
+    }
+    
+    
+    @IBAction func UIDatePicker(_ sender: UIDatePicker) {
+        _ = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        setTime.text = formatter.string(from: datePicker.date)
 
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        view.backgroundColor = UIColor.white
+    }
+    
+    
+    
 }
 
